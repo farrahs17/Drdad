520d63ed69e9f9dde00e5e3e59e79c5b8de28157
Hatem <hattoum96@gmail.com>
2019-08-19 01:56:23 +0200

    creating onClick

--- src/Sidebar/Tools/CreatePatient.js
+++ src/Sidebar/Tools/CreatePatient.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import axios from "axios"
 class CreatePatient extends React.Component{
+    handleClick(){
+
+    }
     render(){
         return(
             <div className="create-patient-div">
-                <button className="create-patient-button">New Patient</button>
+                <button className="create-patient-button" onClick={this.handleClick}>New Patient</button>
             </div>
         )
     }
