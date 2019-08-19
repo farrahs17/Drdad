47c4234a1bd359d15e4dffa4953c4430e9a5e11e
farrahs17 <farrah.shawky.95@gmail.com>
2019-08-19 02:21:53 +0200

    add patient request

--- src/Sidebar/Tools/CreatePatient.js
+++ src/Sidebar/Tools/CreatePatient.js
@@ -1,16 +1,25 @@
-import React from "react"
-import axios from "axios"
-class CreatePatient extends React.Component{
-    handleClick(){
-
-    }
-    render(){
-        return(
-            <div className="create-patient-div">
-                <button className="create-patient-button" onClick={this.handleClick}>New Patient</button>
-            </div>
-        )
-    }
+import React from "react";
+import axios from "axios";
+class CreatePatient extends React.Component {
+  handleClick() {
+    axios
+      .post("http://localhost:5000/add", { name: "name" })
+      .then(result => console.log("success"))
+      .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div className="create-patient-div">
+        <button
+          className="create-patient-button"
+          onClick={this.handleClick}
+          type="submit"
+        >
+          New Patient
+        </button>
+      </div>
+    );
+  }
 }
 
-export default CreatePatient
+export default CreatePatient;
