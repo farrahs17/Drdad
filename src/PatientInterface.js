cbedf0f3bb100aceb77792f0ef10ac094e0c2dd9
farrahs17 <farrah.shawky.95@gmail.com>
2019-08-20 00:22:46 +0200

    working on requests

--- src/PatientInterface.js
+++ src/PatientInterface.js
@@ -1,56 +1,69 @@
 import React from "react";
-import axios from "axios"
+import axios from "axios";
 
-const PatientInterface = React.createContext()
+const PatientInterface = React.createContext();
 
-class Provider extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isLoading: false,
-            currentPatient: "fuck"
+class Provider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      currentPatient: "fuck"
+    };
+  }
+  loadPatient(id) {
+    //request patient data by id and assigns it to state
+    axios
+      .get("http://localhost:5000/get")
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  }
+
+  addVisit() {
+    // let currentPatient = this.state.currentPatient.visits.push("")
+    // this.setState({currentPatient: currentPatient})
+    this.setState(prevState => ({ currentPatient: [...prevState, ""] }));
+  }
+
+  updatePatient() {
+    //POST request
+  }
+
+  deletePatient(id) {
+    //DELETE request
+    axios
+      .post("http://localhost:5000/delete", { id: id })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  }
+
+  createPatient() {
+    axios
+      .post("http://localhost:5000/add", {
+        patient: {
+          name: "name",
+          age: "age",
+          gender: "gender",
+          history: "history",
+          visits: []
         }
-    }
-    loadPatient(id) {
-        //request patient data by id and assigns it to state
-    }
+      })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  }
 
-    addVisit() {
-        // let currentPatient = this.state.currentPatient.visits.push("")
-        // this.setState({currentPatient: currentPatient})
-        this.setState((prevState) => ({ currentPatient: [...prevState, ""] }))
-    }
-
-    updatePatient() {
-        //POST request
-    }
-
-    deletePatient() {
-        //DELETE request
-    }
-
-    createPatient(){
-        axios
-            .post("http://localhost:5000/add", { patient:{
-                name: "name", age: "age", gender: "gender", history: "history", visits: []
-            }})
-            .then(result => console.log(result))
-            .catch(err => console.log(err));
-
-    }
-
-    render(){
-        return(
-            <PatientInterface.Provider
-                value={{
-                    state: this.state,
-                    createPatient: this.createPatient
-                }}
-            >
-                {this.props.children}
-            </PatientInterface.Provider>
-        )
-    }
+  render() {
+    return (
+      <PatientInterface.Provider
+        value={{
+          state: this.state,
+          createPatient: this.createPatient
+        }}
+      >
+        {this.props.children}
+      </PatientInterface.Provider>
+    );
+  }
 }
 
 export { Provider, PatientInterface };
--- src/Sidebar/Tools/CreatePatient.js
+++ src/Sidebar/Tools/CreatePatient.js
@@ -1,25 +1,26 @@
 import React from "react";
 import axios from "axios";
-import {PatientInterface} from "../../PatientInterface"
+import { PatientInterface } from "../../PatientInterface";
 class CreatePatient extends React.Component {
   handleClick(context) {
-    context.createPatient()
+    context.createPatient();
+    // id comes back in data.result._id
   }
   render() {
     return (
-    <PatientInterface.Consumer>
-    {(context=>(
-        <div className="create-patient-div">
-          <button
-            className="create-patient-button"
-            onClick={this.handleClick.bind(this,context)}
-            type="submit"
-          >
-            New Patient
-          </button>
-        </div>
-    ))}
-    </PatientInterface.Consumer>
+      <PatientInterface.Consumer>
+        {context => (
+          <div className="create-patient-div">
+            <button
+              className="create-patient-button"
+              onClick={this.handleClick.bind(this, context)}
+              type="submit"
+            >
+              New Patient
+            </button>
+          </div>
+        )}
+      </PatientInterface.Consumer>
     );
   }
 }
