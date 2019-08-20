cbbdd682aaac0a40bf1f273a5f42575ed75e83bb
farrahs17 <farrah.shawky.95@gmail.com>
2019-08-20 22:53:41 +0200

    addin search request

--- src/Sidebar/SearchArea/SearchArea.js
+++ src/Sidebar/SearchArea/SearchArea.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React from "react";
 import Searchbar from "./Searchbar";
 import SearchResults from "./SearchResults";
+import axios from "axios";
 
-class SearchArea extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            searchResults: []
-        }
-    }
-    //remove _results when requests are implemented
-    populateResults(key,_results){
-        //request to query using the key
-        this.setState({searchResults: _results})
-    }
+class SearchArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: []
+    };
+  }
+  //remove _results when requests are implemented
+  populateResults(key, _results) {
+    //request to query using the key
+    axios
+      .post("http://localhost:5000/search", { searchKey: key })
+      .then(result => {
+        console.log(result);
+        this.setState({ searchResults: result });
+      })
+      .catch(err => console.log(err));
+  }
 
-    render(){
-        return(
-            <div className="search-area">
-                <Searchbar handleSubmit={this.populateResults.bind(this)}/>
-                <SearchResults searchResults = {this.state.searchResults}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search-area">
+        <Searchbar handleSubmit={this.populateResults.bind(this)} />
+        <SearchResults searchResults={this.state.searchResults} />
+      </div>
+    );
+  }
 }
 
-export default SearchArea
+export default SearchArea;
