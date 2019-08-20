85248b291aebd64b23de95fc6d2226f4cd5bfb1c
Hatem <hattoum96@gmail.com>
2019-08-20 23:15:12 +0200

    wiring up search

--- src/Sidebar/SearchArea/SearchArea.js
+++ src/Sidebar/SearchArea/SearchArea.js
@@ -2,6 +2,7 @@ import React from "react";
 import Searchbar from "./Searchbar";
 import SearchResults from "./SearchResults";
 import axios from "axios";
+import {PatientInterface} from "../../PatientInterface"
 
 class SearchArea extends React.Component {
   constructor(props) {
@@ -11,23 +12,24 @@ class SearchArea extends React.Component {
     };
   }
   //remove _results when requests are implemented
-  populateResults(key, _results) {
+  populateResults(key,e) {
+    // e.preventDefault()
     //request to query using the key
     axios
       .post("http://localhost:5000/search", { searchKey: key })
       .then(result => {
         console.log(result);
-        this.setState({ searchResults: result });
+        this.setState({ searchResults: result.data });
       })
       .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <div className="search-area">
-        <Searchbar handleSubmit={this.populateResults.bind(this)} />
-        <SearchResults searchResults={this.state.searchResults} />
-      </div>
+        <div className="search-area">
+          <Searchbar populateResults={this.populateResults.bind(this)} />
+          <SearchResults searchResults={this.state.searchResults} />
+        </div>
     );
   }
 }
--- src/Sidebar/SearchArea/Searchbar.js
+++ src/Sidebar/SearchArea/Searchbar.js
@@ -6,23 +6,19 @@ class Searchbar extends React.Component {
     this.searchKey = "";
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.searchKey);
-    this.props.handleSubmit(this.searchKey, [
-      { name: "pee", id: "4" },
-      { name: "pee", id: "420" },
-      { name: "poo", id: "69" },
-      { name: "poo", id: "46" }
-    ]);
+  handleSubmit(key,e){
+    e.preventDefault()
+    this.props.populateResults(this.searchKey)
+    // console.log(this.searchKey)
   }
+  
   handleChange(e) {
     this.searchKey = e.target.value;
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
+      <form onSubmit={this.handleSubmit.bind(this,this.searchKey)}>
         <input
           className="search-bar"
           onChange={this.handleChange.bind(this)}
