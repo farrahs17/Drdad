c52d9c8556fa3faa9ef118f334caedcab4ad7b79
Hatem <hattoum96@gmail.com>
2019-08-22 00:51:22 +0200

    Toasts and errors

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import PatientView from "./PatientView"
 import LoadingOverlay from "./LoadingOverlay"
 import './App.css';
 import { Provider } from './PatientInterface'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 class App extends React.Component {
   render(){
     return (
       <Provider>
         <div>
+          <ToastContainer autoClose="2000"/>
           <LoadingOverlay/>
           <Sidebar/>
           <PatientView/>
